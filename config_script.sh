08269a70b9a81dce291ee1f9641080afc29288c8
Quentin Fiard <quentin.fiard@polytechnique.org>
2012-03-05 20:41:54 +0100

    Fichiers de configuration

---
+++ config_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git config --global http.proxy http://kuzh.polytechnique.fr:8080
+
+git config filter.sentencebreak.clean "perl -pe \"s/[.]*?(\\?|\\!|\\.|'') /$&%NL%\\n/g unless m/%/||m/^[\\ *\\\\\\]/\""
+git config filter.sentencebreak.smudge "perl -pe \"s/%NL%\n//gm\""
+
+fileArray=($(find . -iname "*.tex"))
+
+for (( i=0; i<${#fileArray[@]}; i++ ));
+do
+    perl -pe "s/%NL%\n//gm" < ${fileArray[$i]} > temp
+    mv temp ${fileArray[$i]}
+done
